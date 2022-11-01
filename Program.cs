390b2cdef9823589f31f483a453c31a1a8a7217d
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-01 22:08:29 +0300

    решено

--- Program.cs
+++ Program.cs
@@ -7,19 +7,36 @@ int N = Convert.ToInt32(Console.ReadLine());
 
 int[] array = new int[N];
 // Рандомный ввод массива
-Random rand = new Random();
-for (int i = 0; i < array.Length; i++)
-   {
-    array[i] = rand.Next(0,9);
-   }
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+     {
+      array[i] = rand.Next(1,9);
+     }
 //Вывод массива 
             Console.Write("Исходный массив - ");
             for (int i = 0; i < array.Length; i++)
             Console.Write(" " + array[i]);
             Console.WriteLine();
 // вычисление количества четных элементов
-int index=0;
-for (int i = 0; i < array.Length; i++)
-if (array[i] % 2 == 0)
-index++;
-Console.Write(index);
+    int index=0;
+    for (int i = 0; i < array.Length; i++)
+    if (array[i] % 2 == 0)
+    index++;
+    Console.Write("количество четных элементов - " + index);
+// создаем новый массив
+int[] NewArray = new int[index];
+int index2=0;
+    for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        NewArray[index2] = array[i];
+        index2++;
+    }
+}
+Console.WriteLine();
+// вывод нового массива
+ Console.Write("Новый массив - ");
+            for (int j = 0; j < index; j++)
+            Console.Write(" " + NewArray[j]);
+ 
